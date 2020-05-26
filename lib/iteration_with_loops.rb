2c0973b013b82b75cd00fe3b6996ea00f905a525
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-26 03:07:09 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,14 @@
 def join_nested_strings(src)
   count = 0
-  strings = []
   while count < src.count do
     inner_count = 0
       while inner_count < src[count].count do
         if src[count][inner_count].class == String
-          strings << src[count][inner_count]
+          src[count][inner_count]
         end
       inner_count +=1
     end
       count +=1
   end      
-  strings.to_s
+
 end
