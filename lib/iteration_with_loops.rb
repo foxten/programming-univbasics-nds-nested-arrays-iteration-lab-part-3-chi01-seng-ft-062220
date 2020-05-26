50c4750c8c8f2618ff686db5d212d9996b80933e
Tenaysia Fox <tenaysiafox@gmail.com>
2020-05-26 03:02:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,7 +1,16 @@
 def join_nested_strings(src)
   count = 0
+  strings = []
   while count < src.count do
-
+    inner_count = 0
+    potential_string = ""
+      while inner_count < src[count].count do
+        if src[count][inner_count].length > potential_string
+          strings << src[count][inner_count]
+        end
+      inner_count +=1
+    end
       count +=1
   end      
+  strings.to_s
 end
